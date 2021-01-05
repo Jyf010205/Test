7d52f6a51b5e246afe0cb17e7ed0d79bceb46ec9
Jyf010205 <785923055@qq.com>
2021-01-05 17:01:38 +0800

    测试

--- src/main/java/com/sgzs/jetkins/controller/TestController.java
+++ src/main/java/com/sgzs/jetkins/controller/TestController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test")
+
+
+
     public String test(){
         return "test";
     }
