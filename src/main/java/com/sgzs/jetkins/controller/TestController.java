577b5cbad932643718beb49d128955babe774f91
Jyf010205 <jianyufeng@agree.com.cn>
2021-01-05 14:44:24 +0800

    新程序

---
+++ src/main/java/com/sgzs/jetkins/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.sgzs.jetkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: jianyufeng
+ * @description:
+ * @date: 2021/1/5 14:38
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
---
+++ src/test/java/com/sgzs/jetkins/JetkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgzs.jetkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
